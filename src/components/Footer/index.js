6e66610017d8ce382d3168a56707d0fd85219da6
Amanda Perry <challenge641@gmail.com>
2022-10-27 15:45:19 -0500

    final touches

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Footer() {
   return (
     <div className="footer d-flex fixed-bottom">
-      <a href="https://www.facebook.com/Challenge641" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faFacebook} size="4x" className="icon" /></a>
-      <a href="https://www.instagram.com/pieceinourtime/" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faInstagram} size="4x" className="icon" /></a>
-      <a href="https://www.linkedin.com/in/amanda-perry-713b0955" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faLinkedin} size="4x" className="icon" /></a>
-      <a href="https://github.com/BeKind-Rewind/" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faGithub} size="4x" className="icon" /></a>
+      <a href="https://www.facebook.com/Challenge641" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faFacebook} size="2x" className="icon" /></a>
+      <a href="https://www.instagram.com/pieceinourtime/" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faInstagram} size="2x" className="icon" /></a>
+      <a href="https://www.linkedin.com/in/amanda-perry-713b0955" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faLinkedin} size="2x" className="icon" /></a>
+      <a href="https://github.com/BeKind-Rewind/" target="blank" className="d-inline-flex mx-2"><FontAwesomeIcon icon={faGithub} size="2x" className="icon" /></a>
     </div>
   )
 }
